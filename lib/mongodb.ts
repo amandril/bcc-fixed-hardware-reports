b7dcae63c85accbaaf078bda88cffb757731c8c6
alex <alexmandrila@gmail.com>
2023-01-27 10:58:47 -0700

    Removed mongo options

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,10 +1,7 @@
 import { MongoClient } from "mongodb";
 
 const uri = process.env.MONGODB_URI
-const options = {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-}
+
 
 let client
 let clientPromise
@@ -15,12 +12,12 @@ if (!process.env.MONGODB_URI) {
 
 if (process.env.NODE_ENV === 'development') {
     if (!global._mongoClientPromise) {
-        client = new MongoClient(uri, options)
+        client = new MongoClient(uri)
         global._mongoClientPromise = client.connect()
     }
     clientPromise = global._mongoClientPromise
 } else {
-    client = new MongoClient(uri, options)
+    client = new MongoClient(uri)
     clientPromise = client.connect()
 }
 
