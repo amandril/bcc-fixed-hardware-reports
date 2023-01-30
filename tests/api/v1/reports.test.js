f6fb7ebd9af825884b79c792dbdf8ea88ff6f012
kao <zichongkao@gmail.com>
2023-01-29 21:22:39 -0700

    Add POST /v1/reports unhappy path test

--- tests/api/v1/reports.test.js
+++ tests/api/v1/reports.test.js
@@ -13,6 +13,9 @@ describe('/v1/reports', () => {
   let db;
 
   beforeAll(async () => {
+    // Create an in-memory Mongo server. 
+    // dbName, ip and port must be in sync with .env.test since
+    // that is where the clientPromise will call to.
     mongoServer = await MongoMemoryServer.create({instance: {
       port: 27017,
       dbName: 'test',
@@ -24,13 +27,18 @@ describe('/v1/reports', () => {
     db = mongoClient.db("test");
   });
 
+  afterEach(async () => {
+    await db.collection("reports").deleteMany({});
+  });
+
   afterAll(async () => {
     await mongoClient.close();
     await mongoServer.stop();
   });
 
   describe('POST request', () => {
-    it('should insert data when receiving well-formed body', async () => {
+
+    it('inserts data when receiving well-formed body', async () => {
       const reportBody = {
         climb: "111222333",
         hardware_type: "bolt",
@@ -53,6 +61,26 @@ describe('/v1/reports', () => {
       expect(allReports[0]).toEqual(
         expect.objectContaining(reportBody)
       )
+    })
+
+    it('throws 400 when encountering missing required fields', async () => {
+      const reportBodyMissingRequiredClimb = {
+        hardware_type: "bolt",
+        assessed_at: 1234567890,
+        description: "rusty spinning bolt"
+      }
+      const { req, res } = createMocks({
+        method: 'POST',
+        body: reportBodyMissingRequiredClimb
+      });
+      await reportHandler(req, res)
+      expect(res._getStatusCode()).toBe(400);
+      expect(JSON.parse(res._getData())["error"]).toEqual(
+        expect.objectContaining({ climb: "Required parameter missing."}),
+      );
+      // No data written.
+      const allReports = await db.collection("reports").find({}).toArray()
+      expect(allReports.length).toBe(0)
     })
   })
 });
