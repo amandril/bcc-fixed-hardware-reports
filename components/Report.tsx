c84835e5743bdbaff0cf593af612496d93b25f00
kao <zichongkao@gmail.com>
2023-02-09 23:28:58 -0700

    Update display to show climb_ids

--- components/Report.tsx
+++ components/Report.tsx
@@ -2,6 +2,8 @@ export default function Report ( {report} ) {
     return (
         <div className="report">
             <div>Report id: {report._id}</div>
+            <div>MP climb id: {report.mp_climb_id}</div>
+            <div>OB climb id: {report.ob_climb_id}</div>
             <div>Name: {report.name}</div>
             <div>Email: {report.email}</div>
             <div>Area: {report.area}</div>
